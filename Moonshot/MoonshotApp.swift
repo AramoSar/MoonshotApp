a8b83bb194a58e8dd32cfe42125927b9d7cfe05a
Aram <aramsaribekyan1@gmail.com>
2024-07-31 14:11:29 +0400

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Aram on 31.07.24.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
